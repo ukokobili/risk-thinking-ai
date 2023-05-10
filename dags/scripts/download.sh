e48870ad988dcbf4406920de07c2df9bfdab38bb
ukokobili <ukokobili.jacob@gmail.com>
2023-05-10 21:15:48 +0100

    bash script

--- dags/scripts/download.sh
+++ dags/scripts/download.sh
@@ -4,8 +4,8 @@
 pip install kaggle
 
 # Set your Kaggle API credentials
-export KAGGLE_USERNAME="jacobukokobili" #$(jq -r '.username' ~/.kaggle/kaggle.json)
-export KAGGLE_KEY="61b5f532a19d8c9f03d309e9b75a02ff" #$(jq -r '.key' ~/.kaggle/kaggle.json)
+export KAGGLE_USERNAME=" " 
+export KAGGLE_KEY=" " 
 
 # Download the dataset using Kaggle API and unzip into the /opt/airflow/etl directory
 kaggle datasets download -d jacksoncrow/stock-market-dataset --unzip -p /opt/airflow/load
